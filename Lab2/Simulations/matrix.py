5c94d280cb43e81e40ba29932f9a1277775dbdbf
Antoine Papillon <antoine.papillon3@gmail.com>
2023-10-21 16:06:32 -0400

    All of the graphs You could ever wish for

--- Lab2/Simulations/matrix.py
+++ Lab2/Simulations/matrix.py
@@ -14,7 +14,7 @@ f2=-75
 f1=150
 phi1 = 25.4  # (mm)
 phi2 = 25.4  # (mm)
-d=0.00345    # (microm)
+taille_pixel=0.00345    # (microm)
 # I define a function for the translation matrix
 def Mt(d):
     '''
@@ -41,18 +41,23 @@ def M_system(t2, t3, L):
     M = Mt(d=L-t3-t2)@Ml(f=75)@Mt(d=t3)@Ml(f=-75)@Mt(d=t2)@Ml(f=150)@Mt(d=1000)
     return M
 plt.close()
+plt.close()
+plt.close()
 L = 180
-L_list=np.linspace(177,201,5)
+L_list=np.linspace(177,200,5)
 
-plt.figure(dpi=96)
+fig_deltaz=plt.figure("deltaz",dpi=96)
+fig_res=plt.figure("res",dpi=96)
+fig_A = plt.figure("A",dpi=96)
+
 # print(M_system(t2,t3, L))
 
 #x = sp.symbols('x') # symbolize x
 t2range = range(0, 76) # range of values for s
 
 
-r2_f_alpha = -np.arctan((phi2 - d) /(2*f3))
-r2_f_y = d/2
+r2_f_alpha = -np.arctan((phi2 - taille_pixel) /(2*f3))
+r2_f_y = taille_pixel/2
 # print(M_tot) # Imprime la matrice totale pour vérification
 
 r2_i_y, r2_i_alpha = sp.symbols('r2_i_y r2_i_alpha') #Définition des variables à résoudre
@@ -71,32 +76,59 @@ r2_i = sp.Matrix([r2_i_y, r2_i_alpha])
     
 
 for L in L_list:
+    # print(f"L={L}")
     # Iteratively solve the problem
     t3_solutions = [] # empty array for solutions
     deltaz_solutions=[] # empty array for solutions
+    A_sol = []
+    res_sol = []
     for t2 in t2range:
         Mtot = M_system(t2, t3, L) # compute the system's matrix
         B = Mtot[0, 1] # get B(x) from system's matrix
         t3_sol = sp.solve(B) # Solve B(x) = 0 Carefull, there may be many solutions...
-        print(t3_sol)
         t3_solutions.append(max(t3_sol)) # add solution to list
     # print(t3_solutions)
     for i,t2 in enumerate(t2range):
         Mtot = M_system(t2, t3_solutions[i], L)
+        #Calcul pour A
+        A=Mtot[0,0]
+        A_sol.append(A)
+        #Calcul pour res
+        res_sol.append(np.abs(taille_pixel/A))
+
+        #Calcul pour la profondeur de champs
         eq = sp.Eq(Mtot * r2_i, sp.Matrix([r2_f_y, r2_f_alpha])) #Définition de l'équation à résoudre
         solution = sp.solve(eq, (r2_i_y, r2_i_alpha)) # Solution de l'angle et hauteur du rayon 2 initial
         r2_i_alpha_value = solution[r2_i_alpha] # Extrait l'angle du vecteur solution
         r2_i_y_value = solution[r2_i_y] #Extrait la hauteur du vecteur solution
         deltaz = abs(2 * r2_i_y_value / math.tan(r2_i_alpha_value)) #Calcul de la résolution avec l'angle et la hauteur
         deltaz_solutions.append(deltaz)
+    plt.figure("A")
+    plt.plot(t2range, A_sol, '.-',label=f"L={L}") 
+
+    plt.figure("res")
+    plt.plot(t2range, 1000*res_sol, '.-',label=f"L={L}") 
+
+    plt.figure("deltaz")
     plt.plot(t2range, deltaz_solutions, '.-',label=f"L={L}")    
 
 
 
+plt.figure("A")
+plt.legend()
+plt.xlabel('t2 [mm]')
+plt.ylabel('Grossissement')
 
-# plot results
-# plt.figure(dpi=96)
-# plt.plot(t2range, deltaz_solutions, '.-')
+plt.figure("res")
+plt.legend()
+plt.xlabel('t2 [mm]')
+plt.ylabel('Résolution [mm]')
+
+plt.figure("deltaz")
+plt.legend()
 plt.xlabel('t2 [mm]')
 plt.ylabel('DOF [mm]')
+
 plt.show()
+
+
